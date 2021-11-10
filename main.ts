b72e95523b194dcdf9ca68278225885adebda677
huskypel <92863057+huskypel@users.noreply.github.com>
2021-11-10 12:49:45 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,6 +21,32 @@ basic.forever(function () {
             # # # # #
             # # # # #
             `)
+    } else if (distance == 15 && distance <= 15) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (distance < 20 && distance <= 25) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (distance == 25 && distance <= 30) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+    	
     }
 })
 basic.forever(function () {
